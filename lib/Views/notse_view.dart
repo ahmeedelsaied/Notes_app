13332cfb35085c9e70b051d1d4b4defa6272d8dc
Ahmed Elsaied <elsaeeda110@gmail.com>
2023-07-17 17:52:16 +0300

    change theme and add notes view

---
+++ lib/Views/notse_view.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+class NotsesView extends StatelessWidget {
+  const NotsesView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
