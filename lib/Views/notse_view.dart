b52b83687987398713d9a56d2dfc52e244b9063e
Ahmed Elsaied <elsaeeda110@gmail.com>
2023-07-17 19:18:55 +0300

    Add body Ui

--- lib/Views/notse_view.dart
+++ lib/Views/notse_view.dart
@@ -1,9 +1,13 @@
 import 'package:flutter/material.dart';
+import 'Widgets/Notes_View_body.dart';
 class NotsesView extends StatelessWidget {
   const NotsesView({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: NotesViewBody(),
+    );
   }
 }
+
